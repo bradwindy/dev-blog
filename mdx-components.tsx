d3988c00f66c83c353e36dff01bc133d3be25d7d
Bradley Windybank <bradley@windybank.net>
2026-01-04 12:06:45 +1300

    refactor: remove custom pre wrapper, use rehype-pretty-code styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,13 +1,9 @@
 import type { MDXComponents } from "mdx/types";
 import { Callout } from "@/components/mdx/callout";
-import { CodeBlock } from "@/components/mdx/code-block";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     Callout,
-    pre: ({ children, ...props }) => (
-      <CodeBlock {...props}>{children}</CodeBlock>
-    ),
   };
 }
